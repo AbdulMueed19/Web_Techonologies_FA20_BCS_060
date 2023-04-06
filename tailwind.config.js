726b5a33d7236d9e5175d2b5c9fe6fdaa243dd6d
Abdul Mueed <amueed_19@hotmail.com>
2023-04-07 04:13:13 +0500

    New

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: [
+        './src/**/*.html',
+        './src/**/*.js',
+        './src/input.css', // Include input.css in content configuration
+    ],
+    // ...
+}
